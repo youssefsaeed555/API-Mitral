efabaccf65525e221853798507de812a60b1004e
youssef saeed <yossefsaid555@gmail.com>
2023-01-27 16:33:51 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const Connect = async () => {
   // mongodb clund connection
+  try{
   const con = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
   console.log(`MongoDB Connected : ${con.connection.host}`);
+  }
 };
 
 module.exports = Connect;
